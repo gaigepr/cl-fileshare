0e24f258326b66d33ab82265075277f4b5115f49
Gaige Pierce-Raison <gaige@chatsubo.net>
2014-09-24 22:34:14 -0700

    Moved http calls into a service.

--- static/js/app.js
+++ static/js/app.js
@@ -11,13 +11,14 @@ fileshareApp.directive('indexEntry', function() {
 });
 
 fileshareApp.controller('ShareCtrl', [
-    '$scope', '$http',
-    function($scope, $http) {
+    '$scope', '$http', '$location', 'ShareService',
+    function($scope, $http, $location, ShareService) {
 
         // gets the last element of a filepath (unix)
         $scope.lastPathElementRegexp = /[^/]+\/*$/;
         $scope.shareRoot = "/home/gaige/Dropbox/";
         $scope.currentPath = "";
+        $scope.crumbs = [];
         $scope.index = [];
 
         // When we get back an object from the api we need to sort it.
@@ -25,54 +26,35 @@ fileshareApp.controller('ShareCtrl', [
         // for display purposes. This could also be done on the lisp side
         // pretty easily I think but that means more data being transfered
 
-        var sortIndex = function(a, b) {
-            // sort by file or directory status
-            if (a.kind > b.kind) return -1;
-            if (a.kind < b.kind) return 1;
-            // sort by name
-            if (a.path < b.path) return -1;
-            if (a.path > b.path) return 1;
+        //$scope.splitCurrentPath = function()
 
-            return 0;
-        };
+        ShareService.loadNewIndex("/home/gaige/Dropbox/").then(function(data) {
+            // get and sort the index
+            $scope.index = data.index.sort(ShareService.sortIndex);
+        });
 
         $scope.handleItem = function(item) {
+            console.log("Reqeust");
             if (item.kind == "Folder") {
-                //console.log("Request to download tree: ", item.path);
-                //$scope.loadNewIndex(item.path);
-                $scope.downloadDirectory(item.path);
+                ShareService.loadNewIndex(item.path).then(function(data) {
+                    $scope.index = data.index.sort(ShareService.sortIndex);
+                    $location.path(item.path);
+                });
             } else if (item.kind == "File") {
                 window.open('/download/' + item.path.replace($scope.shareRoot, ''), '_blank', '');
                 console.log("Request to download file: ", item.path);
             }
         };
 
-        $scope.downloadDirectory = function(path) {
-            $http.post("/download-directory", {
-                "path": path
-            }).success(function(data, status, headers, config) {
-                console.log("SUCCESS: ", data);
-                window.open('/download/' + data.path.replace($scope.shareRoot, ''), '_blank', '');
-                //$scope.currentPath = data.currentPath;
-                //$scope.index = data.index.sort(sortIndex);
-            }).error(function(data, status, headers, config) {
-                console.log("ERROR: ", data, status, headers, config);
-            });
+        $scope.joinCrumbs = function(index) {
+            console.log(index);
+            var path = "/";
+            for (var i = 0; i <= index; i++) {
+                path += $scope.crumbs[i] + "/";
+            }
+            console.log(path);
+            return path;
         };
-
-        $scope.loadNewIndex = function(path) {
-            $http.post("/index", {
-                "path": path
-            }).success(function(data, status, headers, config) {
-                console.log("SUCCESS: ", data);
-                $scope.currentPath = data.currentPath;
-                $scope.index = data.index.sort(sortIndex);
-            }).error(function(data, status, headers, config) {
-                console.log("ERROR: ", data, status, headers, config);
-            });
-        };
-
-        $scope.loadNewIndex("/home/gaige/Dropbox/");
 
     }
 ]);
